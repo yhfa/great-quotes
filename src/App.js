4277246df46c3d5e3bd7607197ca535bc42dd767
youssif hany <youssifhany75@gmail.com>
2021-10-25 23:19:57 +0200

    update quote-form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/quotes" component={AllQuotes} />
         <Route path="/quotes/:quoteId" component={QuoteDetail} />
         <Route path="/new-quote" component={NewQuote} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </Layout>
   );
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "./../components/quotes/QuoteForm";
 
 export default function NewQuote() {
+  const history = useHistory();
   const addQuoteHandler = (quote) => {
     console.log(quote);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
