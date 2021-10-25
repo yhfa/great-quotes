1b12bc5036bf3d62ec2a17d816e45ba9bc6ef363
youssif hany <youssifhany75@gmail.com>
2021-10-25 22:21:42 +0200

    add dummy-data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
 import React from "react";
+import QuoteList from "./../components/quotes/QuoteList";
+
+const DUMMY_DATA = [
+  { id: "q1", author: "jo", text: "Learing react is fun!" },
+  { id: "q2", author: "joe", text: "Learing angular is fun!" },
+  { id: "q3", author: "youssif", text: "Learing vue is fun!" },
+];
 
 export default function AllQuotes() {
-  return <div>All Quotes Page</div>;
+  return <QuoteList quotes={DUMMY_DATA} />;
 }
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,9 @@
 import React from "react";
+import QuoteForm from "./../components/quotes/QuoteForm";
 
 export default function NewQuote() {
-  return <div></div>;
+  const addQuoteHandler = (quote) => {
+    console.log(quote);
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
